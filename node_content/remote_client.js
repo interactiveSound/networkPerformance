09b030977045413d09b876f1cdd9a56e6bad1a4d
einnaew <annie.wang@huskers.unl.edu>
2021-04-19 20:42:53 -0500

    Update remote_client.js

--- node_content/remote_client.js
+++ node_content/remote_client.js
@@ -10,7 +10,7 @@ const maxApi = require("max-api");
 
 let uniqueName = "FriendName";
 let friend = {
-	x: 0, y: 0, name: "Alex", active: false
+	x: 0, y: 0, name: "Annie", active: false
 };
 
 let friends = {};
